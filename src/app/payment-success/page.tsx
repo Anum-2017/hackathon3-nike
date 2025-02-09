b4a609d30d410a2249a0fd7c6f8a564edfc7adb5
Anum Kamal (Web Developer) <87061778+Anum-2017@users.noreply.github.com>
2025-02-09 22:45:09 +0500

    Update page.tsx

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -10,12 +10,12 @@ function PaymentSuccess() {
   const amount = searchParams.get('amount'); // Access query parameter 'amount'
 
   return (
-    <main className="max-w-4xl mx-auto p-10 text-black text-center border m-10 rounded-2xl bg-white">
+    <main className="max-w-4xl mx-auto p-10 text-white text-center border m-10 rounded-2xl bg-black">
       <div className="flex flex-col gap-16 mb-12">
         <h1 className="text-4xl font-extrabold mb-2">PAYMENT RECEIVED</h1>
         <h2 className="text-2xl">Your Order is Confirmed</h2>
 
-        <div className="bg-white p-2 rounded-xl text-black text-3xl font-bold">
+        <div className="bg-white p-2 rounded-xl text-white text-3xl font-bold">
           Payment Success! Total: ₹  {amount}
         </div>
       </div>
